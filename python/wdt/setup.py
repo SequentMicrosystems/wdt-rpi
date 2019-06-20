c572bc6b2da4ec23f656d234e6a099e528c2230e
Alexandru Burcea <olcitu@gmail.com>
2019-06-20 23:38:37 +0300

    Create setup.py

---
+++ python/wdt/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wdt",
+    version="1.0.0",
+    author="Alexandru Burcea",
+    author_email="olcit@gmail.com",
+    description="A set of functions to control Sequent Microsystems Watchdog for Raspberry board",
+	license='MIT',
+    url="https://github.com/SequentMicrosystems/wdt-rpi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2/3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
