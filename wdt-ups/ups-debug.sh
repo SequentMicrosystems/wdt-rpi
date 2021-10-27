95c11493e3e2a4cadf673a79e06cae794b39244f
Alex Jokela <alex@camulus.com>
2021-10-27 09:19:05 -0500

    updating script to only shutdown when power when on battery

--- wdt-ups/ups-debug.sh
+++ wdt-ups/ups-debug.sh
@@ -3,22 +3,28 @@
 #Read the battery voltage
 var1=$(wdt -g vb)
 #Set the voltage threshold in millivolts
-var2=3400
+var2=4000
 date1=$(date -u)
 logfile=/opt/wdt-ups/log/wdt
-if [ $(($var1)) -lt $var2 ]; then
+
+state=$(wdt -g c)
+
+if [ $(($var1)) -lt $var2 ] && [ $(($state)) -eq 0 ]; then
+   
     #Make the wdt not power back on the RPI when on battery
     wdt -rob 0
     #Set the period to a value that allow RPI to finish shutdown until wdt expire
-    wdt -p 30
-    echo $date1 ": Shutdown (var1: "$var1"/var2: "$var2"):" >> $logfile
+    wdt -p 60
+    echo $date1 ": Shutdown (var1: "$(($var1))"/var2: "$var2"):" >> $logfile
+    echo $date1 ": Charging State: " $(($state)) >> $logfile
+    echo $date1 :" --------------- " >> $logfile
     #Shut down the RPI and will back on when main power is back
     shutdown now
 else
     #reload the counter
     wdt -r
 fi    
-    echo $date1 ": Battery (var1: "$var1"/var2: "$var2"):" >> $logfile
-
+    echo $date1 ": Battery (var1: "$(($var1))"/var2: "$var2"):" >> $logfile
+    echo $date1 ": Charging State: " $(($state)) >> $logfile
 
 
